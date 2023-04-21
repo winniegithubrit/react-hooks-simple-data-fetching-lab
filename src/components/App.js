f5cf1c910d8295d39577dbf84e8edcb4c1da1d02
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-04-21 15:22:22 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import { useEffect,useState } from "react";
+
+function App(){
+  
+  const[dogImageUrl,setDogImageUrl]=useState(null)
+useEffect(()=>{
+fetch("https://dog.ceo/api/breeds/image/random")
+.then((response)=>response.json())
+.then((data)=>{
+setDogImageUrl(data.message)
+})
+
+},[])
+return(
+  <div>
+{dogImageUrl ?(
+  <img src={dogImageUrl} alt="A Random Dog"/>
+):(
+  <p>Loading...</p>
+)}
+
+  </div>
+)
+}
+export default App
